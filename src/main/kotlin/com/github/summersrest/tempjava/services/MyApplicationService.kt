27dc51399f56575ba8bfefc3b309b870f3171961
GitHub Action <action@github.com>
2021-10-19 08:33:07 +0000

    Template cleanup

--- src/main/kotlin/com/github/summersrest/tempjava/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/summersrest/tempjava/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.summersrest.tempjava.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.summersrest.tempjava.MyBundle
 
 class MyApplicationService {
 
